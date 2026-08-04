454c48cc4809eac3e6b3bf9d4b709a503bf0137a
ant-xy <ia9167238@gmail.com>
2026-08-04 15:48:40 +0500

    refactor main.py

--- main.py
+++ main.py
@@ -1,99 +1,9 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, Query, status
-import sqlalchemy
-from sqlmodel import select
-
-from models.users import UsersBase, Users, UsersCreate, UsersRead, Payload, UserLogin, Token
-from core.session import SessionDep
-
-from pwdlib import PasswordHash, exceptions
-from pwdlib.hashers.bcrypt import BcryptHasher
-
-from datetime import datetime, timedelta, timezone
-import jwt
-
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-
-password_hash = PasswordHash((BcryptHasher(),))
-
-app = FastAPI()
-
-@app.post("/user/")
-async def create_user(user: UsersCreate, session: SessionDep):
-    user = Users.model_validate(user)
-    user.password = password_hash.hash(user.password)
-
-    try:
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-    except sqlalchemy.exc.IntegrityError as ex:
-        return "Something went wrong, try again later"
-    return user
-
-@app.get("/user/")
-async def check_user(user_id: int, session: SessionDep):
-    user = session.get(Users, user_id)
-
-    if not user:
-        all_users = session.exec(select(Users)).all()
-        payload = Payload(status=200, message="success", data=all_users)
-        return payload
-
-    payload = Payload(status=200, message="success", data=user)
-    return payload
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-random_key = "$2b$12$jySqAint0uiSid0g5/1Fgul396tGSlkrNbVHDEZ6DjTJrOtI3XjMy"
-
-@app.post("/login")
-async def login_user(detail: UserLogin, session: SessionDep):
-    user = session.exec(select(Users).where(Users.username == detail.username)).first()
-
-    if user and password_hash.verify(detail.password, user.password):
-        token_jwt = create_access_token({"user": user.username}, expires_delta=timedelta(days=3))
-        token = Token(jwt=token_jwt, token_type="bearer")
-        return token
-    else:
-        #prevent timebased attacks
-        password_hash.verify(detail.password, random_key)
-        token_jwt = create_access_token({"user": "abc"}, expires_delta=timedelta(days=3))
-        return "Username or password incorrect."
-=======
 from fastapi import FastAPI
 
 app = FastAPI()
 
-=======
-from fastapi import FastAPI
-
-app = FastAPI()
-
->>>>>>> Stashed changes
 from app.api.routers.users import router as user_router
 from app.api.routers.auth import router as auth_router
 
 app.include_router(user_router)
 app.include_router(auth_router)
-
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
